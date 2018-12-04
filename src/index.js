eb4b90286737d77b0a21c9d13182e8aef5d9e453
Harley Oliver <hello@heronamedharley.com>
2018-12-05 08:50:25 +1100

    Added coding-standards and linted accordingly

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Main from './components/Main';
 import Single from './components/Single';
 
-const Root = function() {
-  return (
-    <BrowserRouter>
-      <div>
-        <Route exact path="/" component={Main} />
-        <Route path="/:postId/:postSlug" component={Single} />
-      </div>
-    </BrowserRouter>
-  );
+const Root = function () {
+	return (
+		<BrowserRouter>
+			<div>
+				<Route exact path="/" component={ Main } />
+				<Route path="/:postId/:postSlug" component={ Single } />
+			</div>
+		</BrowserRouter>
+	);
 };
 
-render(<Root />, document.querySelector('#root'));
+render( <Root />, document.querySelector( '#root' ) );
